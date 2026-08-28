55505b5366d7cfa4df780331b067ea4dcdf3b39b
APGSwizzVR <randommonke71@gmail.com>
2026-08-28 12:23:26 +0100

    feat(web): build live VANTA radar interface

--- apps/web/src/react-leaflet.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "react-leaflet" {
-  import type { ComponentType, ReactNode } from "react";
-  export const MapContainer: ComponentType<any>;
-  export const TileLayer: ComponentType<any>;
-  export const Marker: ComponentType<any>;
-  export const Popup: ComponentType<any>;
-  export const Polyline: ComponentType<any>;
-  export const useMap: () => any;
-}
